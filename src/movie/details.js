f233e57c3b6716ab5dcf807a629061927ea320fc
dheamonnica <57338489+dheamonnica@users.noreply.github.com>
2022-01-27 12:33:46 +0700

    update

---
+++ src/movie/details.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+
+// import App from "./App";
+// import style_1 from "./css/application-3347283f99cad225814854f5ea694cf04f29ef25498f29c5d44fcf7ebb202a6b.css";
+// import style_2 from "./css/media-7b01183c64a6819c82ef4f23a3087f1ff79f8de786653d6ffb10a07d2a5b70ef.css";
+// import style_3 from "./css/discover-421028eba2c3eaf5699e02314870662bfc46a923ce94d25af7cfb312dd37ad93.css";
+// import style_4 from "./css/index-22ae5cf76b44e5115ea8049c717a06255873388dd7ae02a2ea653e75829b5eff.css";
+// import style_5 from "./css/2021-20f6213395992680de63c49b86035dc3f6c06bb2575c8c9c3e30a9264da38e63.css";
+
+
+
+class Details extends Component {
+    render() {
+        return (
+            <div class="page_wrap _wrap">
+                aa
+            </div>
+        );
+    }
+}
+
+export default Details;
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+/**
+ * Import all page components here
+ */
+import App from './components/App';
+import MainPage from './components/MainPage';
+import Details from './movies/details';
+
+/**
+ * All routes go here.
+ * Don't forget to import the components above after adding new route.
+ */
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Main} />
+    <Route path="/details" component={Details} />
+  </Route>
+);
